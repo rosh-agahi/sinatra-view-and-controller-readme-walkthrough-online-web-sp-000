75654aeba92ead9bce73c7c0a20eaf2c2a9985ca
Roshanak Agahi <rosh.agahi@gmail.com>
2020-01-17 21:18:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reversed_string
+    
     erb :reversed
   end
 
